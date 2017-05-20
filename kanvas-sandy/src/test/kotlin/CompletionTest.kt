29cb10d8867ea674c94c22e4753614b40fada661
Federico Tomassetti <federico@tomassetti.me>
2017-05-20 17:44:03 +0200

    update Sandy CompletionTest

--- kanvas-sandy/src/test/kotlin/CompletionTest.kt
+++ kanvas-sandy/src/test/kotlin/CompletionTest.kt
@@ -1,45 +1,55 @@
 
 import me.tomassetti.antlr.SandyLexer
 import me.tomassetti.antlr.SandyParser
-import me.tomassetti.kanvas.AutoCompletionContextProvider
-import me.tomassetti.kanvas.EditorContextImpl
-import me.tomassetti.kanvas.TokenTypeImpl
-import me.tomassetti.kanvas.sandyLanguageSupport
+import me.tomassetti.kanvas.*
+import me.tomassetti.kolasu.model.Node
+import org.antlr.v4.runtime.Token
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
+class MyEditorContextImpl(val code: String, val antlrLexerFactory: AntlrLexerFactory, val ast: Node? = null) : EditorContext {
+    override fun cachedAst(): Node? {
+        return ast
+    }
+
+    override fun preceedingTokens(): List<Token> {
+        val lexer = antlrLexerFactory.create(code)
+        return lexer.toList()
+    }
+}
+
 class CompletionTest {
+
+    fun tokenSuggested(code: String) = AutoCompletionContextProvider(SandyParser.ruleNames, SandyParser.VOCABULARY, SandyParser._ATN, debugging = false)
+            .autoCompletionContext(MyEditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals.map { it.first }.toSet()
 
     @test fun emptyFile() {
         val code = ""
-        assertEquals(setOf(TokenTypeImpl(SandyLexer.VAR), TokenTypeImpl(SandyLexer.ID)), AutoCompletionContextProvider(SandyParser.ruleNames,SandyLexer.VOCABULARY,SandyParser._ATN)
-                .autoCompletionContext(EditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals)
+        assertEquals(setOf(TokenTypeImpl(SandyLexer.VAR), TokenTypeImpl(SandyLexer.ID)), tokenSuggested(code))
     }
 
     @test fun afterVar() {
         val code = "var"
-        assertEquals(setOf(TokenTypeImpl(SandyLexer.ID)), AutoCompletionContextProvider(SandyParser.ruleNames,SandyLexer.VOCABULARY,SandyParser._ATN)
-                .autoCompletionContext(EditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals)
+        assertEquals(setOf(TokenTypeImpl(SandyLexer.ID)), tokenSuggested(code))
     }
 
     @test fun afterEquals() {
         val code = "var a ="
         assertEquals(setOf(TokenTypeImpl(SandyLexer.INTLIT), TokenTypeImpl(SandyLexer.DECLIT), TokenTypeImpl(SandyLexer.MINUS)
-                , TokenTypeImpl(SandyLexer.LPAREN), TokenTypeImpl(SandyLexer.ID)), AutoCompletionContextProvider(SandyParser.ruleNames,SandyLexer.VOCABULARY,SandyParser._ATN)
-                .autoCompletionContext(EditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals)
+                , TokenTypeImpl(SandyLexer.LPAREN), TokenTypeImpl(SandyLexer.ID)), tokenSuggested(code))
     }
 
     @test fun afterLiteral() {
         val code = "var a = 1"
-        assertEquals(setOf(TokenTypeImpl(SandyLexer.NEWLINE), TokenTypeImpl(SandyLexer.EOF), TokenTypeImpl(SandyLexer.PLUS), TokenTypeImpl(SandyLexer.MINUS), TokenTypeImpl(SandyLexer.DIVISION), TokenTypeImpl(SandyLexer.ASTERISK)),
-                AutoCompletionContextProvider(SandyParser.ruleNames,SandyLexer.VOCABULARY,SandyParser._ATN)
-                .autoCompletionContext(EditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals)
+        assertEquals(setOf(TokenTypeImpl(SandyLexer.NEWLINE), TokenTypeImpl(SandyLexer.EOF), TokenTypeImpl(SandyLexer.PLUS),
+                TokenTypeImpl(SandyLexer.MINUS), TokenTypeImpl(SandyLexer.DIVISION), TokenTypeImpl(SandyLexer.ASTERISK)),
+                tokenSuggested(code))
     }
 
     @test fun incompleteAddition() {
         val code = "var a = 1 +"
-        assertEquals(setOf(TokenTypeImpl(SandyLexer.LPAREN), TokenTypeImpl(SandyLexer.ID), TokenTypeImpl(SandyLexer.MINUS), TokenTypeImpl(SandyLexer.INTLIT), TokenTypeImpl(SandyLexer.DECLIT)), AutoCompletionContextProvider(SandyParser.ruleNames,SandyLexer.VOCABULARY,SandyParser._ATN)
-                .autoCompletionContext(EditorContextImpl(code, sandyLanguageSupport.antlrLexerFactory)).proposals)
+        assertEquals(setOf(TokenTypeImpl(SandyLexer.LPAREN), TokenTypeImpl(SandyLexer.ID), TokenTypeImpl(SandyLexer.MINUS),
+                TokenTypeImpl(SandyLexer.INTLIT), TokenTypeImpl(SandyLexer.DECLIT)), tokenSuggested(code))
     }
 
 }
